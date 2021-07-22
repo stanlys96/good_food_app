1a2be3287da6b3f6f91831a4ad3353ca0f1fd3b1
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-07-22 11:25:12 +0700

    getById with params

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const MenuController = require('../controllers/MenuController');
 
 router.post('/', MenuController.querySearch);
-router.post('/:category/:id', MenuController.getById);
 router.post('/category', MenuController.getByCategory);
+router.get('/:category/:id', MenuController.getById);
 router.get('/all', MenuController.getAll);
 
 module.exports = router;
